510bd886ae28d55df86e2c00c7c7442ccedda247
sirapaop <sirapaop2545@gmail.com>
2022-12-14 22:23:51 +0700

    link page

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const app = express()
 
+
 app.get("/", (req, res) =>{
     res.send("hello world ^o^")
 })
 
+app.get("*",function(req, res){
+    res.sendFile(__dirname + '/html/error.html')
+})
 app.listen(3000,() =>{
     console.log("start server port [3000]")
 })
